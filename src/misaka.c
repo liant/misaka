62fdc71e750694b103cc2f5faa6000ac48f704c1
liant <liantnix@gmail.com>
2014-01-06 23:58:10 +0800

    首次提交

---
+++ src/misaka.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : misaka.c
+ Author      : liant
+ Version     :
+ Copyright   : bsd
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("Hello World"); /* prints Hello World */
+	return EXIT_SUCCESS;
+}
